935738d340a7a7705ee51b0d11976b4f0a3d74cc
Andrea <andreavlas97@gmail.com>
2023-11-05 22:16:20 +0200

    Add animations to button click

--- game.js
+++ game.js
@@ -2,20 +2,31 @@ const buttonColor = ["red", "blue", "green", "yellow"];
 let gamePattern = [];
 let userClickedPattern = [];
 
+$(".btn").click(function () {
+  let userChosenColor = $(this).attr("id");
+  userClickedPattern.push(userChosenColor);
+  playSound(userChosenColor);
+  animatePress(userChosenColor);
+});
+
 function nextSequence() {
   let randomNumber = Math.floor(Math.random() * 3) + 1;
-  let randomChosenColour = randomNumber[buttonColor];
-  gamePattern.push(randomChosenColour);
-  $("#" + randomChosenColour)
+  let randomChosenColor = randomNumber[buttonColor];
+  gamePattern.push(randomChosenColor);
+  $("#" + randomChosenColor)
     .fadeIn(100)
     .fadeOut(100)
     .fadeIn(100);
+}
 
-  var audio = new Audio("sounds/" + randomChosenColour + ".mp3");
+function playSound(name) {
+  var audio = new Audio("sounds/" + name + ".mp3");
   audio.play();
 }
-$(".btn").click(function () {
-  let userChosenColour = $(this).attr("id");
-  userClickedPattern.push(userChosenColour);
-  console.log(userClickedPattern);
-});
+function animatePress(currentColor) {
+  $("#" + currentColor).addClass("pressed");
+
+  setTimeout(function () {
+    $("#" + currentColor).removeClass("pressed");
+  }, 100);
+}
